ce97d1d834aef15308e3bc2f5955dff40903d339
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-05-04 12:36:50 -0300

    Changed main function logic to choose the user interface.

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,5 @@
 import console.helpMenu
+import controller.updateIPAddress
 import exception.GraphicsIsNotSupportedException
 import gui.MainMenu
 import java.awt.GraphicsEnvironment
@@ -11,27 +12,51 @@ fun main(args: Array<String>) {
     controller.initialize()
 
     if (args.isNotEmpty() && args.contains("--console")) {
-        console.console()
-        controller.finalize()
+        runConsole()
     } else if (args.isNotEmpty() && args.contains("--gui")) {
-        if (GraphicsEnvironment.isHeadless())
-            throw GraphicsIsNotSupportedException()
-        val mainMenu = MainMenu()
-        mainMenu.addCloseOperation {
-            controller.finalize()
-        }
-
-
-    } else if (args.isNotEmpty() && args.contains("--run-once"))
-        controller.updateIPAddress()
-    else if (args.isNotEmpty() && args.any { it.contains("--infinite-loop") }) {
-        var time: Long = 1
-        val timeStr = args.find { it.contains("time=") }
-        timeStr?.let {
-            time = it.substring(it.indexOf("time=") + 5, it.length - 1).toLong()
-        }
-        controller.updateIPAddress(true, time)
+        runGUI()
+    } else if (args.isNotEmpty() && args.contains("--run-once")) {
+        updateIPAddress()
+    } else if (args.isNotEmpty() && args.any { it.contains("--infinite-loop") }) {
+        runDuckDNSIPUpdaterInfiniteLoop(args)
+    } else if (args.isNotEmpty() && args.contains("--help")) {
+        helpMenu()
+    } else if (!GraphicsEnvironment.isHeadless()) {
+        runGUI()
     } else {
         helpMenu()
     }
+}
+
+/**
+ * Runs Duck DNS IP Address Updater in infinite loop.
+ */
+private fun runDuckDNSIPUpdaterInfiniteLoop(args: Array<String>) {
+    var time: Long = 1
+    val timeStr = args.find { it.contains("time=") }
+    timeStr?.let {
+        time = it.substring(it.indexOf("time=") + 5, it.length - 1).toLong()
+    }
+    updateIPAddress(true, time)
+}
+
+/**
+ * Runs Graphical User Interface
+ */
+private fun runGUI() {
+    if (GraphicsEnvironment.isHeadless())
+        throw GraphicsIsNotSupportedException()
+
+    val mainMenu = MainMenu()
+    mainMenu.addCloseOperation {
+        controller.finalize()
+    }
+}
+
+/**
+ * Runs non-graphical user interface
+ */
+private fun runConsole() {
+    console.console()
+    controller.finalize()
 }
